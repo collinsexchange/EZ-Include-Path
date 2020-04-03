d3fc16ed95a72ae24f5e9cd10f3d814d67e4fda3
collinsexchange <collinsexchange@yahoo.com>
2020-04-03 16:07:56 -0400

    Update index.php
    updated path

--- to/index.php
+++ to/index.php
@@ -1,7 +1,7 @@
 <?php
 include_once "../path.php";
 //Example 1:
-// include_once Path::match(this, $_SERVER['DOCUMENT_ROOT']."/path/to/connecting_class.php");
+// include_once Path::match(this, $_SERVER['DOCUMENT_ROOT']."/EZ-Include-Path/to/connecting_class.php");
 
 //Example 2:
 include_once Path::match(this, connecting_class);
